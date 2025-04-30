186ad416d8b66bb9ed8de5b5427bce75422b23d1
ahad-jiva <ahadjiva15@gmail.com>
2025-04-30 11:41:57 -0700

    init

---
+++ fib.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// input: an integer k and a prime p, where p is congruent to ±2 mod 5
+// output: the (k+1)th Fibonacci number mod p
+
+int main(int argc, char* argv[]){
+
+    int fib_l = atoi(argv[1]) + 1;
+    int mod = atoi(argv[2]);
+
+    int fib[fib_l];
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= fib_l; i++){
+        fib[i] = (fib[i-1] + fib[i-2]) % mod;
+    }
+
+    printf("%d\n", fib[fib_l]);
+    
+
+}
---
+++ flt.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+// input: an integer p
+// first: check that the input is congruent to ±2 mod 5
+// if so, run Fermat primality test with base 2
+
+int flt(int argc, char *argv[]){
+
+    int p = atoi(argv[1]); //TODO: bignum
+
+    // check first condition
+    int test = p % 5;
+    if ((test != 2) || (test != 3)){
+        printf("Integer is not congruent to ±2 mod 5");
+        return 0;
+    }
+    else
+    {
+        int flt = 2 << p - 1;
+        int mod = flt % p;
+        if (mod == 1){
+            printf("Integer passes FLT");
+            return 1;
+        }
+    }
+}
